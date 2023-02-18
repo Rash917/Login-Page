d03953a01f622a22e6a617a85e1cdf8503fddb9e
Rashmi Kumari <rashmi_kumari@persistent.com>
2023-02-18 21:40:23 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {  Button, Form, TextInput } from '@carbon/react';
 
-function App() {
+import { useState } from "react";
+import "../src/App.css"
+
+
+
+
+function App() { 
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    if(username === password.split("").reverse().join("")) {
+      alert("Password is correct")
+    } else {
+      alert("Password is incorrect")
+    }
+    setUsername("");
+    setPassword("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{Align:"center",marginTop:"100px",marginLeft:"350px",marginRight:"350px", fontSize:"30px" ,background:"white",paddingBottom:"30px"}}>
+     
+     <div style={{textAlign:"center",padding:"20px",backgroundColor:"burlywood"}}>Login Page</div>
+     <Form  onSubmit={e=>onFormSubmit(e)}>
+
+     <TextInput className="app1" 
+        onChange={e=>setUsername(e.target.value)}
+        value={username}
+        id="username"
+        invalidText="Invalid error message."
+        labelText="Username"
+        placeholder="Enter Username"
+        required
+        $text-placeholder="Red"
+        />
+        
+      <TextInput className="app2"
+        id="password"
+        value={password}
+        type="password"
+        labelText="Password"
+        placeholder="Enter Password"
+        required
+        onChange={e=>setPassword(e.target.value)}
+        onInvalid={h=>h.target.setCustomValidity("Password is incorrect!")}
+        // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+
+      />
+      <Button type="submit"  className="app3"  >Submit</Button>
+      </Form>
     </div>
   );
 }
 
 export default App;
+
